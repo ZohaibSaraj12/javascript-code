045a8fb3f047b3059729292a851cefa0647a8e91
Muhammad Zohaib <zohaibsaraj10@gmail.com>
2026-02-01 16:17:26 +0500

    All files

---
+++ DatatypesInDetail.js
@@ -0,0 +1,21 @@
+"use stric"; // good practice to indicate, that we are treating js with new version
+
+// alert(3 + 3) // used in browser runtime environment, I am learning NODE.JS
+
+
+
+
+// number -> 2^53
+// bingint ->
+// string -> ""
+// boolean -> true/false
+// null -> standalone value
+// undefined
+// symbol
+
+
+let a = 10
+let b = "Zohaib"
+console.log(typeof a," ", typeof b);
+console.log(typeof undefined, typeof null);
+
